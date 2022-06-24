c05a4f6031ec475c7e2a2ef565078ac6bfdce328
Dio Reyhans <58151425+dioreyhans@users.noreply.github.com>
2022-06-24 13:10:08 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
+    <h1>TES</h1>
       <GlobalProvider>
         <Router>
           <Routes>
